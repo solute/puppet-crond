5331632c40157410e37ccd1ba939824587216520
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2013-02-12 10:40:42 +0100

    Style fixes to tests/init.pp and a comment

--- tests/init.pp
+++ tests/init.pp
@@ -1,4 +1,10 @@
+# The following snippet creates a file /etrc/cron.d/testfubbel
+# which will execute /bin/echo foo every minute
+#
+# e.g.
+# 1 *   *   *   *   /bin/echo foo
+
 crond { 'testfubbel':
-    minute   => 1,
-    command => '/bin/echo foo'
+    minute      => 1,
+    command     => '/bin/echo foo'
 }
